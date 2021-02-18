7b3c7ef1aa203df9478b2d50d97f1bea6d6b501a
Gabriel Terwesten <gabriel@terwesten.net>
2021-02-18 15:51:51 +0100

    dynamicaly load libraries from bundle

--- packages/cbl_flutter/lib/cbl_flutter.dart
+++ packages/cbl_flutter/lib/cbl_flutter.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:cbl_ffi/cbl_ffi.dart';
+import 'package:path/path.dart' as p;
 
 /// Locates and returns the [Libraries] shipped by this package (`cbl_flutter`),
 /// handling the differences between platforms.
@@ -10,10 +11,18 @@ Libraries flutterLibraries() {
       cbl: LibraryConfiguration.executable(),
       cblDart: LibraryConfiguration.executable(),
     );
-  } else if (Platform.isAndroid || Platform.isLinux) {
+  } else if (Platform.isAndroid) {
     return Libraries(
       cbl: LibraryConfiguration.dynamic('libCouchbaseLiteC'),
       cblDart: LibraryConfiguration.dynamic('libCouchbaseLiteDart'),
+    );
+  } else if (Platform.isLinux) {
+    final bundlePath = p.dirname(Platform.resolvedExecutable);
+    final libDir = p.join(bundlePath, 'lib');
+    return Libraries(
+      cbl: LibraryConfiguration.dynamic(p.join(libDir, 'libCouchbaseLiteC')),
+      cblDart:
+          LibraryConfiguration.dynamic(p.join(libDir, 'libCouchbaseLiteDart')),
     );
   } else {
     throw UnsupportedError('This platform is not supported.');
